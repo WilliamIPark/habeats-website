e7b4f95e2d31b14fffa59d1b28a5c3d5a2ae45a2
William Park <williampark@hotmail.co.uk>
2019-04-02 22:41:56 +0100

    Updates

---
+++ precache-manifest.5616e080b37d4ba24b9eceb62c3d4943.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "473a273e018e0fa43f01a12922a1a46d",
+    "url": "/static/media/sketchyPhone.473a273e.svg"
+  },
+  {
+    "revision": "88da5d50f181ab550a4e05a345d5ba94",
+    "url": "/static/media/preview-phone.88da5d50.png"
+  },
+  {
+    "revision": "c938d242e6158aece912e53c5b2e3c33",
+    "url": "/static/media/logoCircle.c938d242.svg"
+  },
+  {
+    "revision": "244fe07b0d3d0ae300a2cad8baf70e8c",
+    "url": "/static/media/logo.244fe07b.svg"
+  },
+  {
+    "revision": "c846e66c4a761ca47418e9877bc9bb79",
+    "url": "/static/media/heroSplit.c846e66c.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "48d79c0eb8d4b8ffca2a",
+    "url": "/static/js/main.48e649ab.chunk.js"
+  },
+  {
+    "revision": "2a180a2d7456531e7db8",
+    "url": "/static/js/2.3188d79b.chunk.js"
+  },
+  {
+    "revision": "48d79c0eb8d4b8ffca2a",
+    "url": "/static/css/main.c6e91c1c.chunk.css"
+  },
+  {
+    "revision": "669044259a3c67c9900ad240af8105b6",
+    "url": "/index.html"
+  }
+];
